337d705beb19d6a316f00a93a17665e9eca798ec
Luiz Henrique Chagas de Lima <luiz.limah22@gmail.com>
2026-04-21 06:04:06 -0400

    feat: implementa dashboard com informações financeiras

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -5,7 +5,7 @@ const dashboardService = new DashboardService();
 
 export class DashboardController {
   async overview(_req: Request, res: Response) {
-    const result = await dashboardService.getOverview();
-    return res.status(200).json(result);
+    const data = await dashboardService.overview();
+    return res.status(200).json(data);
   }
 }
